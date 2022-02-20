6430d0978dcdc40c0250998c57a8cbd5e2f94f4b
Alexander Pishvanov <citikot@users.noreply.github.com>
2022-02-20 11:50:28 +0200

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(n)
+{
+  if( n < 0 || n > 12) {
+        throw new RangeError("The argument must be between 0 and 12.")
+  }
+  
+  if ( n === 0 ) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+  
+}
